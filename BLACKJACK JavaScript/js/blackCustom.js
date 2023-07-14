3fad5a1519b9996c9b3c1a11ea6b69a1165f4cb2
Muhammad Noman <m.nomanarif97@gmail.com>
2023-07-14 20:47:53 +0500

    New Repos and Codes

---
+++ BLACKJACK JavaScript/js/blackCustom.js
@@ -0,0 +1,36 @@
+// BlackJack Code
+let firstCard = 10;
+let secondCard = 7;
+let allCards = [firstCard, secondCard];
+let sum = firstCard + secondCard;
+let hasBlackJack = false;
+let isAlive = true;
+let messageLine = "";
+let messageEl = document.getElementById("message-el");
+let sumEl = document.getElementById("sum-el");
+// let sumEl = document.querySelector("#sum-el");
+let cardEl = document.getElementById("card-el");
+function startGame(){
+    renderGame();
+}
+function renderGame(){
+    cardEl.textContent = "Cards: " + allCards[0] + " " + allCards[1];
+    sumEl.textContent = "Sum: " + sum;
+    if (sum <= 20) {
+        messageLine = "Do you want to draw another Card?";
+    } else if (sum === 21) {
+        messageLine = "You've got a Blackjack!";
+        hasBlackJack = true;
+    } else {    
+        messageLine = "You're out of the game!";
+        isAlive = false;
+    }
+    messageEl.textContent = messageLine;
+    
+}
+function newCard(){
+    console.log("Drawing a new card from the deck!");
+    let thirdCard = 4;
+    sum += thirdCard;
+    renderGame();
+}
---
+++ Other JS PRACTICE CODE/js/customOther.js
@@ -0,0 +1,61 @@
+// let firstCard = 6;
+// let secondCard = 10;
+// let sum = firstCard + secondCard;
+
+// if (sum < 21) {
+//     console.log("Do you want to draw another Card? 🙂");
+// } else if (sum === 21) {
+//     console.log("Wohoo! You've got a Blackjack! 🥳");
+// } else {
+//     console.log("You're out of the game! 😭");
+// }
+
+// Operator JS Code
+// let ageFirst = 22;
+// if (ageFirst < 21) {
+//     console.log("You can not enter the club!");
+// } else {
+//     console.log("Welcome");
+// }
+// let ageSecond = 19;
+// if (ageSecond < 21) {
+//     console.log("You can not enter the club!");
+// } else {
+//     console.log("Welcome");
+// }
+
+// let ageFirst = 98;
+
+// if (ageFirst < 100) {
+//     console.log("Not elegible");
+// } else if (ageFirst === 100){
+//     console.log("Here is your birthday card from the King!");
+// } else {
+//     console.log("Not elegible, you have already gotten one");
+// }
+
+// let ageSecond = 100;
+
+// if (ageSecond < 100) {
+//     console.log("Not elegible");
+// } else if (ageSecond === 100){
+//     console.log("Here is your birthday card from the King!");
+// } else {
+//     console.log("Not elegible, you have already gotten one");
+// }
+
+// let ageThird = 102;
+
+// if (ageThird < 100) {
+//     console.log("Not elegible");
+// } else if (ageThird === 100){
+//     console.log("Here is your birthday card from the King!");
+// } else {
+//     console.log("Not elegible, you have already gotten one");
+// }
+// Operator JS Code End
+
+// Array JS Code Practice
+
+
+
